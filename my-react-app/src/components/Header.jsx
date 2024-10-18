15e2468d9c133606ab30633d265301c4c0d42148
Kimberly Bridges <kbridges.prsvr@gmail.com>
2024-10-18 00:04:06 -0500

    add change

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,12 +1,18 @@
 // src/components/layout/Header.jsx
 import React from 'react';
-import '../Styles/Header.css'; // CSS file for styling
+import oilImage from '../../images/oilimage.jpg'; // Adjust the path as needed
+import '../Styles/Header.css'; // Import the CSS file for styling
 
 const Header = () => {
   return (
     <header className="app-header">
       <h1>Rich Essential Oils</h1>
       <p>Discover the Benefits of Natural Essential Oils</p>
+
+      {/* Image Container with the Oil Image */}
+      <div className="image-container">
+        <img src={oilImage} alt="Oil" className="fading-image" />
+      </div>
     </header>
   );
 };
