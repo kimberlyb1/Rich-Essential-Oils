9f231983ce5e72a9ccdb8fecca64f8ef0533bb74
Kimberly Bridges <kbridges.prsvr@gmail.com>
2024-10-18 09:17:12 -0500

    add change

--- my-react-app/src/components/OilList.jsx
+++ my-react-app/src/components/OilList.jsx
@@ -1,13 +1,30 @@
 // src/components/OilList.jsx
 import React from 'react';
-import OilCard from './OilCard'; // Assuming you have an OilCard component for individual oils
+import './OilList.css'; // Make sure this CSS file exists.
 
-const OilList = ({ oils }) => {
+// Optional import: If you have an OilCard component
+import OilCard from './OilCard'; // Comment out if not using
+
+const OilList = ({ oils, onClick }) => {
   return (
     <div className="oil-list">
-      {oils.map((oil, index) => (
-        <OilCard key={index} oil={oil} />
-      ))}
+      {oils.map((oil) =>
+        // Use OilCard if it exists, otherwise render oil directly
+        OilCard ? (
+          <OilCard key={oil.id} oil={oil} onClick={() => onClick(oil)} />
+        ) : (
+          <div
+            key={oil.id}
+            className="oil-item"
+            onClick={() => onClick(oil)}
+            data-aos="fade-up"
+          >
+            <img src={oil.image} alt={oil.name} className="oil-image" />
+            <h3>{oil.name}</h3>
+            <p>{oil.description}</p>
+          </div>
+        )
+      )}
     </div>
   );
 };
