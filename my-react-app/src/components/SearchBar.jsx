6d5ac894e2ed77d36204851fe87fafd284a639c7
Kimberly Bridges <kbridges.prsvr@gmail.com>
2024-10-19 23:36:37 -0500

    add changes

--- my-react-app/src/components/SearchBar.jsx
+++ my-react-app/src/components/SearchBar.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import '../Styles/SearchBar.css';
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search for essential oils..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-    </div>
+    <input
+      type="text"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      placeholder="Search..."
+    />
   );
 };
 
